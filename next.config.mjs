710dd39d8d4922fb39df8bb28e2ec950f16aeb14
Niraj Basyal <niraj.basyal2054@gmail.com>
2026-05-12 12:27:58 -0600

    Configure Sentry for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: true,
+  widenClientFileUpload: false,
+  hideSourceMaps: true,
+  disableLogger: true,
+});
