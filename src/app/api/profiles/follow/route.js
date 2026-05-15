a58c798e36b9d9a7b992e7cdd900f4f79b403145
Niraj Basyal <niraj.basyal2054@gmail.com>
2026-05-15 12:04:35 -0600

    Accept all Supabase UUIDs in follow API

--- src/app/api/profiles/follow/route.js
+++ src/app/api/profiles/follow/route.js
@@ -32,8 +32,8 @@ function cleanText(value) {
 }
 
 function isValidUuid(value) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
-    String(value || "")
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(
+    String(value || "").trim()
   );
 }
 
