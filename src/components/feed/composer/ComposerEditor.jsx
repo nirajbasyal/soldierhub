7d8db5bb010207ce8f3f7b61c41caa8fe2b2d5fa
Niraj Basyal <niraj.basyal2054@gmail.com>
2026-06-08 16:13:54 -0600

    Lazy load TipTap composer editor

--- src/components/feed/composer/ComposerEditor.jsx
+++ src/components/feed/composer/ComposerEditor.jsx
@@ -1,6 +1,15 @@
 "use client";
 
-import TipTapComposerEditor from "./TipTapComposerEditor";
+import dynamic from "next/dynamic";
+
+const TipTapComposerEditor = dynamic(() => import("./TipTapComposerEditor"), {
+  ssr: false,
+  loading: () => (
+    <div className="min-h-[160px] w-full rounded-2xl border border-[#D9E2EC] bg-[#F7FAFD] p-4 text-sm font-semibold text-[#7B8794]">
+      Loading editor...
+    </div>
+  ),
+});
 
 export default function ComposerEditor(props) {
   return <TipTapComposerEditor {...props} />;
